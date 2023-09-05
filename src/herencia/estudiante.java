e56bd54478abc7100a53e3b57f2b48346b344509
Aracely7 <u21203160@utp.edu.pe>
2023-09-05 16:41:53 -0500

    Agregando el proyecto a GitHub

---
+++ src/herencia/estudiante.java
@@ -0,0 +1,26 @@
+
+package herencia;
+
+public class estudiante extends persona {
+    private int codigoEstudiante;
+    private float notafinal;
+     
+    
+    public estudiante (String nombre,String apellido,int edad, int codigoEstudiante,float notafinal){
+ 
+        super(nombre,apellido,edad);
+        this.codigoEstudiante= codigoEstudiante;
+        this.notafinal=notafinal;
+    }
+    
+public void mostrarDatos(){
+   System.out.println("Nombre:"+ getNombre()+
+           "\nApellido:"+getApellido()+
+           "\nEdad:"+getEdad()+
+           "\nCodigoEstudiante:"+codigoEstudiante+
+           "\nNotafinal:"+notafinal);
+    
+}
+}
+ 
+    
---
+++ src/herencia/principal.java
@@ -0,0 +1,15 @@
+
+package herencia;
+
+
+public class principal {
+
+   
+    public static void main(String[] args) {
+       estudiante estudiante=new estudiante ("Aracely","lara",12,121,15.6f);
+        estudiante.mostrarDatos();
+       
+ 
+    }
+    
+}
